f916f1634a9247ee5fd20e930c3dcf93cc22f1d4
aliulug <keremaliulug@gmail.com>
2013-10-25 14:01:21 +0300

    New implementation acceptance tests

--- KateTrainReservation4/TicketOfficeNUnitTest.cs
+++ KateTrainReservation4/TicketOfficeNUnitTest.cs
@@ -8,66 +8,68 @@ namespace KataTrainReservation
     [TestFixture]
     public class TicketOfficeNUnitTest
     {
-
-        //[Test]
-        //public void TestThat_BookingASeatOnATrainThatDoesNotExistReturnsNull()
-        //{
-        //    // TODO: write this code
-        //    var ticketOffice = new TicketOffice();
-        //    var reservation = ticketOffice.MakeReservation(new ReservationRequest("fake train", 1));
-
-        //    Assert.That(reservation, Is.Null);
-        //}
-
-        //[Test]
-        //public void TestThat_BookingASeatOnAnExistingTrainReturnsARealReservation()
-        //{
-        //    var ticketOffice = new TicketOffice();
-        //    var reservation = ticketOffice.MakeReservation(new ReservationRequest("real train", 1));
-
-        //    Assert.That(reservation, Is.Not.Null);
-        //}
-
         [Test]
-        public void TestThat_BookingASeatOnATrainThatDoesNotExistReturnsNull2()
+        public void TestThat_BookingASeatOnATrainThatDoesNotExistShouldNotWork()
         {
             var userInterface = new UserInterface();
+            
+            var reservation = userInterface.BookSeat("fake train", 1);
 
-            Reservation reservation = userInterface.BookSeat("fake train", 1);
-
-            Assert.That(reservation, Is.Null);
+            Assert.That(reservation, Is.False);
         }
 
         [Test]
         public void TestThat_BookingASeatOnAnExistingTrainReturnsARealReservation()
         {
             var userInterface = new UserInterface();
+            var trainForBooking = new TrainForBooking("real train");
+            trainForBooking.AddCoach(new TrainCoach(10));
+            userInterface.AddTrain(trainForBooking);
 
-            Reservation reservation = userInterface.BookSeat("real train", 1);
+            var reservation = userInterface.BookSeat("real train", 1);
 
-            Assert.That(reservation, Is.Not.Null);
+            Assert.That(reservation, Is.True);
         }
 
         [Test]
-        public void TestThat_BookingASeatOnAnExistingFullTrainReturnsNull()
+        public void TestThat_BookingASeatOnAnExistingFullTrainShouldNotWork()
         {
-            Mockery mockery = new Mockery();
-            ITrainList trainList = mockery.NewMock<ITrainList>();
-            var userInterface = new UserInterface(trainList);
-
-            //userInterface.AddTrain("real train", 0);
+            var mockery = new Mockery();
+            var trainList = mockery.NewMock<ITrainList>();
+            var userInterface = new UserInterface();
 
             Stub.On(trainList).Method("DoesTrainHaveSeats").With("real full train", 2).Will(Return.Value(false));
 
+            var reservation = userInterface.BookSeat("real full train", 2);
 
-            Reservation reservation = userInterface.BookSeat("real full train", 2);
-
-            Assert.That(reservation, Is.Null);
+            Assert.That(reservation, Is.False);
         }
-    }
 
-    public interface ITrainList
-    {
-        bool DoesTrainHaveSeats(string trainName, int numberOfSeats);
+        [Test]
+        public void TestThat_BookingASeatOnAnExistingEmptyTrainReturnsARealReservation()
+        {
+            var userInterface = new UserInterface();
+            var trainForBooking = new TrainForBooking("real train");
+            trainForBooking.AddCoach(new TrainCoach(10));
+            userInterface.AddTrain(trainForBooking);
+
+            var reservation = userInterface.BookSeat("real train", 2);
+
+            Assert.That(reservation, Is.True);
+        }
+
+        [Test]
+        public void TestThat_BookingSeatsOnATrainWithOneCouchAndTenSeatsDoesNotWorkWhenMoreThan70PercentRequested()
+        {
+            var userInterface = new UserInterface();
+            var trainCoach = new TrainCoach(10);
+            var train = new TrainForBooking("train 1");
+            train.AddCoach(trainCoach);
+
+            userInterface.AddTrain(train);
+            var reservation = userInterface.BookSeat("train 1", 8);
+
+            Assert.That(reservation, Is.False);
+        }
     }
 }
--- KateTrainReservation4/UserInterface.cs
+++ KateTrainReservation4/UserInterface.cs
@@ -9,23 +9,38 @@ namespace KateTrainReservation4
 {
     public class UserInterface
     {
-        private readonly ITrainList _trainList;
+//        private readonly ITrainList _trainList;
+        private readonly List<TrainForBooking> _trains = new List<TrainForBooking>(); 
 
         public UserInterface()
         { }
         
-        public UserInterface(ITrainList trainList)
+        //public UserInterface(ITrainList trainList)
+        //{
+        //    _trainList = trainList;
+        //}
+
+        public bool BookSeat(string trainName, int numberOfSeats)
         {
-            _trainList = trainList;
+            //if (_trainList == null)
+            //    return null;
+
+            //if (_trainList.DoesTrainHaveSeats(trainName, numberOfSeats))
+            //    return new Reservation();
+            var train = getTrainWithName(trainName);
+            if (train == null)
+                return false;
+            return train.BookSeats(numberOfSeats);
         }
 
-        public Reservation BookSeat(string trainName, int numberOfSeats)
+        private TrainForBooking getTrainWithName(string trainName)
         {
-            if (_trainList == null)
-                return null;
-            if (_trainList.DoesTrainHaveSeats(trainName, numberOfSeats))
-                return new Reservation();
-            return null;
+            return _trains.FirstOrDefault(trainForBooking => trainForBooking.TrainName == trainName);
+        }
+
+        public void AddTrain(TrainForBooking train)
+        {
+            _trains.Add(train);
         }
     }
 }
